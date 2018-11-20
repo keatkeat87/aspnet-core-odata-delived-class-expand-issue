d8a4c812a0b5be5e0b06527c40a8d9480b32d157
Derrick Yam <keatkeat87@live.com>
2018-11-20 22:19:04 +0800

    ok

--- ODataDelivedExpandIssue/Controllers/UsersController.cs
+++ ODataDelivedExpandIssue/Controllers/UsersController.cs
@@ -21,7 +21,8 @@ namespace ODataDelivedExpandIssue.Controllers
         [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.Select | AllowedQueryOptions.Format | AllowedQueryOptions.Count | AllowedQueryOptions.Filter | AllowedQueryOptions.OrderBy | AllowedQueryOptions.Skip | AllowedQueryOptions.Top | AllowedQueryOptions.Expand, MaxExpansionDepth = 3)]
         public IActionResult Get()
         {
-            return Ok(Db.Characters);
+            return Ok(Db.Admins); // this not work!
+            //return Ok(Db.Characters); // this will work!
         }
     }
 }
